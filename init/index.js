8960a148154ed9deb1337c8ec31c31bfbd522c99
Sambit-px <sambit2745@gmail.com>
2025-06-05 02:20:27 +0530

    new

--- init/index.js
+++ init/index.js
@@ -5,10 +5,8 @@ const initData = require("./data.js"); /* ./ -> current directory */
 const Listing = require("../models/listing.js"); /* ../ -> parent directoy */
 
 const mongo_url = process.env.ATLASDB_URL;
-const owner_id1 = "682d612bb8339304cd84525e"; //from mongoDB
-const owner_id2 = "67e006af800d9a3a4938942a"; //from mongo atlas
-
-const owner_id = owner_id1 || owner_id2;
+//const owner_id1 = "682d612bb8339304cd84525e"; //from mongoDB
+//const owner_id2 = "67e006af800d9a3a4938942a"; //from mongo atlas
 
 
 main()
@@ -25,7 +23,7 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
-  initData.data = initData.data.map((obj) => ({ ...obj, owner: owner_id }));
+  initData.data = initData.data.map((obj) => ({ ...obj, owner: "67e006af800d9a3a4938942a" }));
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
